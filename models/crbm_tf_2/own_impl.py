37db3cee0bda21ae4e29ef90758dce1888943393
Alexander Kuebel <alexander.kuebel@code.berlin>
2021-07-22 10:05:35 -0400

    Implement Gibb's sampling

--- models/crbm_tf_2/own_impl.py
+++ models/crbm_tf_2/own_impl.py
@@ -21,13 +21,46 @@ class CRBM:
     def __init__(self: CRBM):
         # height, width, in_channels, out_channels/filter_num
         kernel_initial_val = tf.keras.initializers.truncated_normal(stddev=0.01)(shape=(12, 1, 1, 24))
+        h_bias_initial_val = tf.keras.initializers.Constant(0)(shape=(1,10,1,24))
+        v_bias_initial_val = tf.keras.initializers.Constant(0)(shape=(1,120,1,1))
         self.kernel = tf.Variable(name='weights', initial_value=kernel_initial_val, dtype=tf.dtypes.float32)
+        self.h_bias = tf.Variable(name='h_bias', initial_value=h_bias_initial_val, dtype=tf.dtypes.float32)
+        self.v_bias = tf.Variable(name='v_bias', initial_value=v_bias_initial_val, dtype=tf.dtypes.float32)
 
+    # =======================================================================================================
+    # Gibb's
+    # =======================================================================================================
+    def gibbs_sampling(self: CRBM, input, n=1):
+        V0 = input
+        Q0 = self._propagate_forward(input)
+        ret = self.sample_forward(Q0)
+        for _ in range(n):
+            VN = self.sample_backward((ret))
+            QN = self._propagate_backward(VN)
+            ret = self.sample_backward(QN)
+        return V0, Q0, VN, QN
+
+    # =======================================================================================================
+    # Sampling logic
+    # =======================================================================================================
     def _propagate_forward(self: CRBM, input):
-        return tf.nn.conv2d(input, self.kernel, [12, 1], padding='SAME')
+        foo = tf.nn.conv2d(input, self.kernel, [12, 1], padding='SAME')
+        return tf.sigmoid(foo + self.h_bias)
+
+    def sample_forward(self: CRBM, hidden_activations):
+        hidden_shape = (1, 10, 1, 24) # TODO: change this later to [self.batch_size, height, width, channels]
+        samples_rand_uniform = tf.random.uniform(hidden_shape)
+        choices_for_true = tf.ones(hidden_shape)
+        choices_for_false = tf.zeros(hidden_shape)
+        return tf.where(samples_rand_uniform - hidden_activations < 0, choices_for_true, choices_for_false) # TODO make this non-binary later
 
     def _propagate_backward(self: CRBM, hidden):
-        return tf.nn.conv2d_transpose(hidden, self.kernel, (1, 120, 1, 1), [1, 12, 1, 1])
+        foo = tf.nn.conv2d_transpose(hidden, self.kernel, (1, 120, 1, 1), [1, 12, 1, 1])
+        return tf.sigmoid(foo + self.v_bias)
+
+    def sample_backward(self: CRBM, visible_activations):
+        # TODO: might want to add sampling here. Usually people do not sample on backward but author did
+        return visible_activations
 
 # ===========================================================================================================
 # Evaluation
@@ -40,5 +73,6 @@ inputs = tf.random.uniform(
 )
 
 c = CRBM()
-hidden = c._propagate_forward(inputs); print(hidden.shape)
-visible = c._propagate_backward(hidden); print(visible.shape)
+res = c.gibbs_sampling(inputs)
+for r in res:
+    print(r.shape)
